b875199a73701b04c2080bb3342a0238bdbece73
Reeve <reevelau@gmail.com>
2014-08-17 10:43:59 +0800

    make little change

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var express = require('express')
+var express = require('express');
+var request = require('request');
+var through = require('through');
 var app = express();
 var pg = require('pg');
 
+
+var tr = through(
+                function write(data){
+                  console.log('queuing num bytes: ' + data.length);
+                  this.queue(data);
+                },
+                function end(){
+                  this.queue(null);
+                });
+/*
 pg.connect(process.env.DATABASE_URL, function(err, client,done){
     client.query('SELECT * FROM mytable', function(err, result){
         done();
@@ -9,11 +21,13 @@ pg.connect(process.env.DATABASE_URL, function(err, client,done){
         console.log(result.rows);
     });
 });
-
+*/
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/', function(req, response) {
+        request('http://hk.yahoo.com').pipe(tr).pipe(response);
+        /*
         console.log('this is a log from my app');
         response.write('<html><head/><body>');
         response.write('<strong>Hello World!</strong>');
@@ -22,6 +36,7 @@ app.get('/', function(request, response) {
         response.write('</strong>');
         response.write('</body></html>');
         response.end();
+        */
 })
 
 app.listen(app.get('port'), function() {

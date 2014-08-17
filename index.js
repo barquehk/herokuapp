ed3b3a9c173ee6ef9ad1af90e6907c5097780ccd
Reeve <reevelau@gmail.com>
2014-08-17 09:09:58 +0800

    checking for testing

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express')
 var app = express();
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client,done){
+    client.query('SELECT * FROM mytable', function(err, result){
+        done();
+        if(err) return console.error(err);
+        console.log(result.rows);
+    });
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('<html><head/><body>')
+  response.send('<strong>Hello World!</strong>')
+  response.send('</body></html>')
 })
 
 app.listen(app.get('port'), function() {

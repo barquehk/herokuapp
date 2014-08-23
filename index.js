95df712e27302ed677a572e3e59371ac9a636cb7
Reeve <reevelau@gmail.com>
2014-08-23 10:39:43 +0800

    adding jsdom dependency

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var express = require('express')
   , conf = require('./conf')
   , everyauthRoot = './';
 
+// this is for testing the support of jsdom in heroku
+var jsdom = require('jsdom');
+jsdom.env (
+    'http://nodejs.org/dist/',
+    ['http://code.jquery.com/jquery.js'],
+    function(errors, window){
+      console.log('there have been', window.$('a').length, 'nodejs releases!');
+    }    
+    );
+// this is for testing
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
